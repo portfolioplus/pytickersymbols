bcf106d0f031fcbcbed585496dc7d7c385bc1fa4
CDieck <christoph.dieck@gmail.com>
2019-07-06 13:20:22 +0200

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 from setuptools import setup, find_packages
 
 EXCLUDE_FROM_PACKAGES = ['test', 'test.*', 'test*']
-VERSION = '1.0.3'
+VERSION = '1.0.4'
 
 def get_requirements(requirements):
     with open(requirements) as requirement_file:
