903dc749098ff67eb53dc60d6b5410a738c713b6
CDieck <christoph.dieck@gmail.com>
2019-07-07 12:50:15 +0200

    moved to tox and pytest.
    Fixed typo in industry section.
    
    Added index to yahoo symbol converter.

--- src/pytickersymbols/__init__.py
+++ src/pytickersymbols/__init__.py
@@ -4,11 +4,10 @@
 
  Copyright 2019 Christoph Dieck
 
- Use of this source code is governed by a GNU General Public License v3 or later that can be
- found in the LICENSE file.
+ Use of this source code is governed by a GNU General Public License v3 or
+ later that can be found in the LICENSE file.
 """
 import os
-
 import yaml
 
 
@@ -16,7 +15,9 @@ class PyTickerSymbols:
 
     def __init__(self):
         self.__stocks = None
-        yaml_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'stocks.yaml')
+        yaml_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "data", "stocks.yaml"
+        )
         with open(yaml_path) as stocks:
             self.__stocks = yaml.safe_load(stocks)
 
@@ -25,21 +26,22 @@ class PyTickerSymbols:
         Returns all available indices
         :return: list of index names
         """
-        return self.__get_sub_items('indices')
+        return self.__get_sub_items("indices")
 
     def get_all_industries(self):
         """
         Returns all available industries
         :return: list of industries
         """
-        return self.__get_sub_items('industries')
+        return self.__get_sub_items("industries")
 
     def get_all_countries(self):
         """
         Returns all available countries
         :return: list of country names
         """
-        return list(set([stock['country'] for stock in self.__stocks['companies']]))
+        items = [stock["country"] for stock in self.__stocks["companies"]]
+        return list(set(items))
 
     def get_stocks_by_index(self, index):
         """
@@ -47,24 +49,24 @@ class PyTickerSymbols:
         :param index: name of index
         :return: list of stocks
         """
-        return self.__get_items('indices', index)
-    
+        return self.__get_items("indices", index)
+
     def get_yahoo_ticker_symbols_by_index(self, index):
         """
         Returns a list with yahoo ticker symbols who belongs to given index.
         :param index: name of index
         :return: list of yahoo ticker symbols
         """
-        my_items = self.__get_items('indices', index)
+        my_items = self.__get_items("indices", index)
         return self.__filter_data(my_items, False, True)
-    
+
     def get_google_ticker_symbols_by_index(self, index):
         """
         Returns a list with google ticker symbols who belongs to given index.
         :param index: name of index
         :return: list of google ticker symbols
         """
-        my_items = self.__get_items('indices', index)
+        my_items = self.__get_items("indices", index)
         return self.__filter_data(my_items, True, False)
 
     def get_stocks_by_industry(self, industry):
@@ -73,7 +75,7 @@ class PyTickerSymbols:
         :param industry: name of index
         :return: list of stocks
         """
-        return self.__get_items('industries', industry)
+        return self.__get_items("industries", industry)
 
     def get_stocks_by_country(self, country):
         """
@@ -81,15 +83,48 @@ class PyTickerSymbols:
         :param country: name of country
         :return: list of stocks
         """
-        return [stock for stock in self.__stocks['companies'] if isinstance(country, str) and stock['country'].lower() == country.lower()]
 
-    def __get_items(self, key, search):
-        stocks = [stock for stock in self.__stocks['companies']
-                      for my_item in stock[key] if isinstance(search, str) and search.lower() == my_item.lower()]
+        def __valid(cou, st_cou):
+            return isinstance(cou, str) and st_cou.lower() == cou.lower()
+
+        return [
+            stock
+            for stock in self.__stocks["companies"]
+            if __valid(country, stock["country"])
+        ]
+
+    def index_to_yahoo_symbol(self, index_name):
+        """
+        Returns the yahoo symbol for index name.
+        :param country: name of index
+        :return: yahoo symbol
+        """
+        yahoo_symbol = None
+        for index_item in self.__stocks["indices"]:
+            if index_item['name'] == index_name:
+                yahoo_symbol = index_item['yahoo']
+                break
+        return yahoo_symbol
+
+    def __get_items(self, key, val):
+        stocks = [
+            stock
+            for stock in self.__stocks["companies"]
+            for item in stock[key]
+            if isinstance(val, str) and val.lower() == item.lower()
+        ]
         return stocks
 
     def __get_sub_items(self, key):
-        sub_items = list(set([item for stock in self.__stocks['companies'] for item in stock[key]]))
+        sub_items = list(
+            set(
+                [
+                    item
+                    for stock in self.__stocks["companies"]
+                    for item in stock[key]
+                ]
+            )
+        )
         return sub_items
 
     @staticmethod
@@ -97,10 +132,10 @@ class PyTickerSymbols:
         ticker_list = []
         for stock in stocks:
             sub_list = []
-            for symbol in stock['symbols']:
-                if google and 'google' in symbol and symbol['google'] != '-': 
-                    sub_list.append(symbol['google'])
-                if yahoo and 'yahoo' in symbol and symbol['yahoo'] != '-':
-                    sub_list.append(symbol['yahoo'])
+            for symbol in stock["symbols"]:
+                if google and "google" in symbol and symbol["google"] != "-":
+                    sub_list.append(symbol["google"])
+                if yahoo and "yahoo" in symbol and symbol["yahoo"] != "-":
+                    sub_list.append(symbol["yahoo"])
             ticker_list.append(sub_list)
         return ticker_list
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -24,9 +24,9 @@ class TestLib(unittest.TestCase):
         assert stock_data
         indices = stock_data.get_all_indices()
         assert indices
-        assert 'DAX' in indices
-        assert 'SDAX' in indices
-        assert 'MDAX' in indices
+        assert "DAX" in indices
+        assert "SDAX" in indices
+        assert "MDAX" in indices
         # duplicates are not allowed
         for index in indices:
             assert len([index_tmp for index_tmp in indices if index_tmp == index]) == 1
@@ -40,12 +40,17 @@ class TestLib(unittest.TestCase):
         assert stock_data
         countries = stock_data.get_all_countries()
         assert countries
-        assert 'Germany' in countries
-        assert 'Netherlands' in countries
-        assert 'Sweden' in countries
+        assert "Germany" in countries
+        assert "Netherlands" in countries
+        assert "Sweden" in countries
         # duplicates are not allowed
         for country in countries:
-            assert len([country_tmp for country_tmp in countries if country_tmp == country]) == 1
+            assert (
+                len(
+                    [country_tmp for country_tmp in countries if country_tmp == country]
+                )
+                == 1
+            )
 
     def test_industry(self):
         """
@@ -56,12 +61,14 @@ class TestLib(unittest.TestCase):
         assert stock_data
         industries = stock_data.get_all_industries()
         assert industries
-        assert 'Computer Hardware' in industries
-        assert 'Gold' in industries
-        assert 'Banking Services' in industries
+        assert "Computer Hardware" in industries
+        assert "Gold" in industries
+        assert "Banking Services" in industries
         # duplicates are not allowed
         for industry in industries:
-            assert len([tmp_item for tmp_item in industries if tmp_item == industry]) == 1
+            assert (
+                len([tmp_item for tmp_item in industries if tmp_item == industry]) == 1
+            )
 
     def test_stocks_by_index(self):
         """
@@ -78,13 +85,13 @@ class TestLib(unittest.TestCase):
         assert len(stocks) == 0
         stocks = stock_data.get_stocks_by_index(22)
         assert len(stocks) == 0
-        stocks = stock_data.get_stocks_by_index('DAX')
+        stocks = stock_data.get_stocks_by_index("DAX")
         assert stocks
         assert len(stocks) == 30
         for stock in stocks:
             is_in_dax = False
-            for index in stock['indices']:
-                if 'DAX' in index:
+            for index in stock["indices"]:
+                if "DAX" in index:
                     is_in_dax = True
             assert is_in_dax
 
@@ -103,12 +110,12 @@ class TestLib(unittest.TestCase):
         assert len(stocks) == 0
         stocks = stock_data.get_stocks_by_country(22)
         assert len(stocks) == 0
-        stocks = stock_data.get_stocks_by_country('Israel')
+        stocks = stock_data.get_stocks_by_country("Israel")
         assert stocks
         assert len(stocks) >= 1
         for stock in stocks:
             is_in_israel = False
-            if 'Israel' == stock['country']:
+            if "Israel" == stock["country"]:
                 is_in_israel = True
             assert is_in_israel
 
@@ -127,12 +134,12 @@ class TestLib(unittest.TestCase):
         assert len(stocks) == 0
         stocks = stock_data.get_stocks_by_industry(22)
         assert len(stocks) == 0
-        stocks = stock_data.get_stocks_by_industry('Basic Materials')
+        stocks = stock_data.get_stocks_by_industry("Basic Materials")
         assert stocks
         for stock in stocks:
             is_in_basic = False
-            for industry in stock['industries']:
-                if 'Basic Materials' in industry:
+            for industry in stock["industries"]:
+                if "Basic Materials" in industry:
                     is_in_basic = True
             assert is_in_basic
 
@@ -151,7 +158,7 @@ class TestLib(unittest.TestCase):
         assert len(google_tickers) == 0
         google_tickers = stock_data.get_google_ticker_symbols_by_index(22)
         assert len(google_tickers) == 0
-        google_tickers = stock_data.get_google_ticker_symbols_by_index('DAX')
+        google_tickers = stock_data.get_google_ticker_symbols_by_index("DAX")
         assert google_tickers
         yahoo_tickers = stock_data.get_yahoo_ticker_symbols_by_index(None)
         assert len(yahoo_tickers) == 0
@@ -161,7 +168,7 @@ class TestLib(unittest.TestCase):
         assert len(yahoo_tickers) == 0
         yahoo_tickers = stock_data.get_yahoo_ticker_symbols_by_index(22)
         assert len(yahoo_tickers) == 0
-        yahoo_tickers = stock_data.get_yahoo_ticker_symbols_by_index('DAX')
+        yahoo_tickers = stock_data.get_yahoo_ticker_symbols_by_index("DAX")
         assert yahoo_tickers
         test_list = [google_tickers, yahoo_tickers]
         for test_item in test_list:
@@ -175,14 +182,23 @@ class TestLib(unittest.TestCase):
         """
         stock_data = PyTickerSymbols()
         assert stock_data
-        y_tickers = stock_data.get_yahoo_ticker_symbols_by_index('DAX')
+        y_tickers = stock_data.get_yahoo_ticker_symbols_by_index("DAX")
         for tickers in y_tickers:
             for ticker in tickers:
-                yahoo = pdr.get_data_yahoo(ticker, '2019-07-01', '2019-07-05', interval='d')
+                yahoo = pdr.get_data_yahoo(
+                    ticker, "2019-07-01", "2019-07-05", interval="d"
+                )
                 assert yahoo is not None
-                assert 'Close' in yahoo
-                assert len(yahoo['Close']) > 0
+                assert "Close" in yahoo
+                assert len(yahoo["Close"]) > 0
+    
+    def test_index_to_yahoo(self):
+        stock_data = PyTickerSymbols()
+        assert stock_data
+        assert '^GDAXI' == stock_data.index_to_yahoo_symbol('DAX')
+        assert '^SDAXI' ==  stock_data.index_to_yahoo_symbol('SDAX')
+        assert '^MDAXI' == stock_data.index_to_yahoo_symbol('MDAX')
+        assert '^SSMI' == stock_data.index_to_yahoo_symbol('Switzerland 20')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
