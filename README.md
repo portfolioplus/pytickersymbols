97d06fa83321455b4e7b6cb97670a724eec6c84d
Slash Gordon <slash.gordon.dev@gmail.com>
2019-08-01 21:14:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+[![Build Status](https://travis-ci.org/portfolioplus/pytickersymbols.svg?branch=master)](https://travis-ci.org/portfolioplus/pytickersymbols)
+[![Coverage Status](https://coveralls.io/repos/github/portfolioplus/pytickersymbols/badge.svg?branch=master)](https://coveralls.io/github/portfolioplus/pytickersymbols?branch=master)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/5ef7d89e40b7491db63ea9753e01851c)](https://www.codacy.com/app/cdieck88/pytickersymbols?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=portfolioplus/pytickersymbols&amp;utm_campaign=Badge_Grade)
+# pytickersymbols
+
+pytickersymbols provides access to google and yahoo ticker symbols for all stocks of the following indices:
+
+- [x] AEX
+- [x] BEL 20
+- [x] CAC 40
+- [x] DAX
+- [x] DOW JONES
+- [x] FTSE 100
+- [x] IBEX 35
+- [x] MDAX
+- [x] NASDAQ 100
+- [x] OMX Helsinki 15
+- [x] OMX Helsinki 25
+- [x] OMX Stockholm 30
+- [x] S&P 100
+- [x] S&P 500
+- [x] SDAX
+- [x] SMI
+- [x] TECDAX
+
+# quick start
+
+Get all countries, indices and industries as follow:
+```python
+from pytickersymbols import PyTickerSymbols
+
+stock_data = PyTickerSymbols()
+countries = stock_data.get_all_countries()
+indices = stock_data.get_all_indices()
+industries = stock_data.get_all_industries()
+```
+
+You can select all stocks of an index as follow:
+```python
+from pytickersymbols import PyTickerSymbols
+
+stock_data = PyTickerSymbols()
+german_stocks = stock_data.get_stocks_by_index('DAX')
+uk_stocks = stock_data.get_stocks_by_index('FTSE 100')
+
+```
+
+# issue tracker
+
+[https://github.com/portfolioplus/pytickersymbols/issuese](https://github.com/portfolioplus/pytickersymbols/issues")
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" pytickersymbols
+  Copyright 2019 Slash Gordon
+  Use of this source code is governed by an MIT-style license that
+  can be found in the LICENSE file.
+"""
+
+from setuptools import setup, find_packages
+
+EXCLUDE_FROM_PACKAGES = ['test', 'test.*', 'test*']
+VERSION = '1.0.11'
+
+def get_requirements(requirements):
+    with open(requirements) as requirement_file:
+        content = requirement_file.readlines()
+    content = [x.strip() for x in content]
+    return content
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pytickersymbols",
+    version=VERSION,
+    author="Slash Gordon",
+    author_email="slash.gordon.dev@gmail.com",
+    py_modules=['pytickersymbols'],
+    package_dir={'': 'src'},
+    description="The lib provides ticker symbols for yahoo and google finance.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/portfolioplus/pytickersymbols",
+    packages=find_packages('src', exclude=EXCLUDE_FROM_PACKAGES),
+    package_data={'': ['data/*.yaml']},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Office/Business :: Financial :: Investment',
+    ],
+)
