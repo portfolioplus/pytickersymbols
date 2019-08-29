0813f52b080ee4ac95fb4b4c42e4bd381104eac5
Slash <slash.gordon.dev@gmail.com>
2019-08-29 14:21:41 +0200

    Fixed Johnson Controls International yahoo symbol.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@
 from setuptools import setup, find_packages
 
 EXCLUDE_FROM_PACKAGES = ['test', 'test.*', 'test*']
-VERSION = '1.1.6'
+VERSION = '1.1.7'
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
