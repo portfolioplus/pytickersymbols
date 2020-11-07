207bd06c2c81182c3daf576266a044e61f81c00f
Slash Gordon <slash.gordon.dev@gmail.com>
2020-11-07 08:06:35 +0100

    Added statics and moved code (#50)
    The scanner code was moved to a separate repository.

--- tools/yaml2json.py
+++ tools/yaml2json.py
@@ -4,9 +4,9 @@ import os
 
 # convert yaml file to json
 input_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                          'stocks.yaml')
+                          '..', 'stocks.yaml')
 output_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                           'src', 'pytickersymbols', 'data', 'stocks.json')
+                           '..', 'src', 'pytickersymbols', 'data', 'stocks.json')
 
 os.makedirs(os.path.dirname(os.path.realpath(output_path)), exist_ok=True)
 
