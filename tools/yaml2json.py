4759ee23cf4e8e3fdf8fa4c063062f61c5c407a0
Christoph Dieck <christoph.dieck@deutschebahn.com>
2022-11-21 08:34:27 +0100

    feat: fix symbols

--- tools/yaml2json.py
+++ tools/yaml2json.py
@@ -12,4 +12,15 @@ os.makedirs(os.path.dirname(os.path.realpath(output_path)), exist_ok=True)
 
 with open(output_path, 'w') as out_file:
     with open(input_path, 'r') as in_file:
-        json.dump(yaml.safe_load(in_file), out_file, ensure_ascii=False)
+        stock = yaml.safe_load(in_file)
+        for idx, company in enumerate(stock["companies"]):
+            while True:
+                stop = True
+                for idy, symbol in enumerate(company["symbols"]):
+                    if "skip" in symbol and symbol["skip"]:
+                        stock["companies"][idx]["symbols"].pop(idy)
+                        stop = False
+                        break
+                if stop:
+                    break
+        json.dump(stock, out_file, ensure_ascii=False)
