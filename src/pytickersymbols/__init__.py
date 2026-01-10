f5d0e8adeb112d0c58288cc19dfdae0a402fd30f
SlashGordon <slash.gordon.dev@gmail.comn>
2026-01-10 12:18:59 +0000

    Release pytickersymbols v1.17.4

--- src/pytickersymbols/__init__.py
+++ src/pytickersymbols/__init__.py
@@ -13,7 +13,7 @@ import itertools
 from typing import Any, Dict, Iterable, Iterator, List, Optional
 from pytickersymbols.indices_data import INDICES
 
-__version__ = "1.17.3"
+__version__ = "1.17.4"
 
 
 class Statics:
